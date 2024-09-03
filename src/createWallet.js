c159b650cc80cbefbe148d61c4023f085c036357
ebnzr <ebenezerfontenele@gmail.com>
2024-09-02 22:29:25 -0300

    Primeiro commit

---
+++ src/createWallet.js
@@ -0,0 +1,34 @@
+// Importando as depedências
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+// Definir a rede
+// Bitcoin rede principal: mainnet
+// Bitcoin rede de teste: testnet
+const network = bitcoin.networks.testnet
+
+// Derivação de carteiras HD (Hierarchical Deterministic)
+const path = `m/49'/1'/0'/0`
+
+// Criando o mnemonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+// Criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+// Criando uma conta (par de pvt e pub keys)
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+// Gerando um endereço
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log("Carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Chave privada: ", node.toWIF())
+console.log("Seed: ", mnemonic)
